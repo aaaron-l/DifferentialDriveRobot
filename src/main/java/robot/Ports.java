496dadfdf571d1f79017bd499d404de875a3138b
Aaron Lin <aaronlin1001@gmail.com>
2024-12-03 21:39:37 -0500

    fixed a part of code in Ports.java

--- src/main/java/robot/Ports.java
+++ src/main/java/robot/Ports.java
@@ -10,7 +10,7 @@ public final class Ports {
   public static final class Drive {
     public static final int RIGHT_LEADER = 2;
     public static final int RIGHT_FOLLOWER = 3;
-    public static final int LEFT_LEADER = 2;
-    public static final int LEFT_FOLLOWER = 3;
+    public static final int LEFT_LEADER = 4;
+    public static final int LEFT_FOLLOWER = 5;
   }
 }
