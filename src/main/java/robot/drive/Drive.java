ebf52be44415584b885c7035da2c9871b8f5f199
Aaron Lin <aaronlin1001@gmail.com>
2024-12-19 14:43:56 -0500

    did some more of differntial drive up to override simulation periodic as there is no variable seconds

--- src/main/java/robot/drive/Drive.java
+++ src/main/java/robot/drive/Drive.java
@@ -4,17 +4,24 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.List;
 import java.util.function.DoubleSupplier;
+
+import robot.Constants;
 import robot.Ports;
+import robot.Robot;
+import robot.drive.DriveConstants;
 import robot.drive.DriveConstants.FF;
 import robot.drive.DriveConstants.PID;
 
@@ -53,6 +60,18 @@ public class Drive extends SubsystemBase {
     gyro.reset();
 
     odometry = new DifferentialDriveOdometry(new Rotation2d(), 0, 0, new Pose2d());
+
+    // ...
+    driveSim =
+    new DifferentialDrivetrainSim(
+        DCMotor.getMiniCIM(2),
+        DriveConstants.GEARING,
+        DriveConstants.MOI,
+        DriveConstants.DRIVE_MASS,
+        DriveConstants.WHEEL_RADIUS,
+        DriveConstants.TRACK_WIDTH,
+        DriveConstants.STD_DEVS);
+    //...
   }
 
   private void drive(double leftSpeed, double rightSpeed) {
@@ -61,19 +80,22 @@ public class Drive extends SubsystemBase {
 
     final double realLeftSpeed = leftSpeed * DriveConstants.MAX_SPEED;
     final double realRightSpeed = rightSpeed * DriveConstants.MAX_SPEED;
-
+        
     final double leftFeedforward = feedforward.calculate(realLeftSpeed);
     final double rightFeedforward = feedforward.calculate(realRightSpeed);
-
-    final double leftPID = leftPIDController.calculate(leftEncoder.getVelocity(), realLeftSpeed);
-    final double rightPID =
+      
+    final double leftPID = 
+      leftPIDController.calculate(leftEncoder.getVelocity(), realLeftSpeed);
+    final double rightPID = 
         rightPIDController.calculate(rightEncoder.getVelocity(), realRightSpeed);
-
+    
     double leftVoltage = leftPID + leftFeedforward;
     double rightVoltage = rightPID + rightFeedforward;
-
+  
     leftLeader.setVoltage(leftVoltage);
     rightLeader.setVoltage(rightVoltage);
+
+    driveSim.setInputs(leftVoltage, rightVoltage);
   }
 
   private void updateOdometry(Rotation2d rotation) {
@@ -83,6 +105,8 @@ public class Drive extends SubsystemBase {
   @Override
   public void periodic() {
     updateOdometry(gyro.getRotation2d());
+    updateOdometry(Robot.isReal() ? gyro.getRotation2d() :  
+    driveSim.getHeading());
   }
 
   public Pose2d pose() {
@@ -95,6 +119,11 @@ public class Drive extends SubsystemBase {
 
   private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(FF.kS, FF.kV);
 
-  private final PIDController leftPIDController = new PIDController(PID.kP, PID.kI, PID.kD);
-  private final PIDController rightPIDController = new PIDController(PID.kP, PID.kI, PID.kD);
+    private final PIDController leftPIDController =
+      new PIDController(PID.kP, PID.kI, PID.kD);
+  private final PIDController rightPIDController =
+      new PIDController(PID.kP, PID.kI, PID.kD);
+
+
+  private final DifferentialDrivetrainSim driveSim;
 }
--- src/main/java/robot/drive/DriveConstants.java
+++ src/main/java/robot/drive/DriveConstants.java
@@ -1,5 +1,10 @@
 package robot.drive;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N7;
+
 public class DriveConstants {
   public static final double WHEEL_RADIUS = 0.08; // Meters
   public static final double CIRCUMFERENCE = 2.0 * Math.PI * WHEEL_RADIUS;
@@ -20,4 +25,9 @@ public class DriveConstants {
   }
 
   public static final double MAX_SPEED = 2; // Meters per second
+
+  public static final double TRACK_WIDTH = 0.7112; // Meters
+  public static final double MOI = 7.5;
+  public static final double DRIVE_MASS = 60.0; //kg
+  public static final Matrix<N7, N1> STD_DEVS = VecBuilder.fill(0, 0, 0, 0, 0, 0, 0);
 }
